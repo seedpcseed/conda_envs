f2dc80b70da84119786b290249d4bbac29dcf035
housw <housw2010@gmail.com>
2019-01-23 13:50:34 -0800

    backup conda env

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # conda_envs
+
+* To back up: 
+
+```
+./backup_conda_env.sh
+```
+
+* To create your conda env:
+
+```
+conda env create --name ${ENV_NAME} --file ${YAML_FILE}
+``` 
---
+++ backup_conda_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+time_stamp=`date "+%m-%d-%Y %H:%M:%S"`
+
+conda env list | awk '{print $1}' | grep -v -E "^$|^#" | while read -r env_name
+do
+    yml_file=${env_name}.yml
+    echo "[$time_stamp] Exporting conda environment $env_name to $yml_file ..."
+    conda env export -n $env_name > $yml_file
+done
+
+echo "[$time_stamp] Done!"
