5ee1a6da563c7d0faf4825ebe6200489d21e0799
housw <housw2010@gmail.com>
2019-02-10 14:12:35 -0800

    no builds

--- backup_conda_env.sh
+++ backup_conda_env.sh
@@ -6,7 +6,7 @@ conda env list | awk '{print $1}' | grep -v -E "^$|^#" | while read -r env_name
 do
     yml_file=${env_name}.yml
     echo "[$time_stamp] Exporting conda environment $env_name to $yml_file ..."
-    conda env export -n $env_name > $yml_file
+    conda env export --no-builds -n $env_name > $yml_file
 done
 
 echo "[$time_stamp] Done!"
